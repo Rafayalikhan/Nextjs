788579835429485625a72b5a8aa162f9cf5b57be
Bimrely <bim@bimrely.com>
2025-02-12 09:24:55 +0500

    first commit

--- src/app/(plain)/(authentication)/auth/sign-in/components/useSignIn.js
+++ src/app/(plain)/(authentication)/auth/sign-in/components/useSignIn.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -14,6 +14,13 @@ const useSignIn = () => {
   const { push } = useRouter();
   const { showNotification } = useNotificationContext();
   const queryParams = useQueryParams();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      push('/feed/home'); // Already logged in, redirect to dashboard
+    }
+  }, [push]);
 
   const loginFormSchema = yup.object({
     email: yup.string().email('Please enter a valid email').required('Please enter your email'),
@@ -38,11 +45,10 @@ const useSignIn = () => {
 
       const { token, user } = response.data;
 
-      // Store the token and user in localStorage
       localStorage.setItem('token', token);
       localStorage.setItem('user', JSON.stringify(user));
+      localStorage.setItem('isAuthenticated', 'true');
 
-      // Redirect to the intended page or default to '/feed/home'
       push(queryParams['redirectTo'] ?? '/feed/home');
 
       showNotification({
